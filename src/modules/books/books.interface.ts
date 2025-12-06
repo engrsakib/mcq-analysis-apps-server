28eaf99c1d85402cdb1e51684324156ec61dabf1
engrsakib <snazmussakib01@gmail.com>
2025-12-06 19:49:59 +0600

    buy url and book intreface ready

---
+++ src/modules/books/books.interface.ts
@@ -0,0 +1,15 @@
+export type IBook = {
+  book_number?: number;
+  title: string;
+  description?: string;
+  is_published: boolean;
+  price: number;
+  sold_platform?: BOOK_PLATFORM_ENUMS;
+  buy_url?: string;
+};
+
+export enum BOOK_PLATFORM_ENUMS {
+  ROKOMARI = "rokomari",
+  WAFI_LIFE = "wafi_life",
+  OTHERS = "others",
+}
