881cd718dc76acfb1656e9f9a64f3353b077311a
engrsakib <snazmussakib01@gmail.com>
2025-12-08 01:14:49 +0600

    update and data ss

---
+++ src/modules/exam/exam.service.ts
@@ -0,0 +1,101 @@
+import { BarcodeService } from "@/lib/barcode";
+import { ExamModel } from "./exam.model";
+import { GUIDELINE_STATUS } from "../guideline/guideline.interface";
+import { IExam } from "./exam.interface";
+
+class Service {
+  async createExam(payload: Partial<IExam>): Promise<IExam> {
+    try {
+      const examNumber = BarcodeService.generateEAN13();
+
+      const examData = {
+        ...payload,
+        exam_number: examNumber,
+        is_published: false,
+        is_started: false,
+        is_completed: false,
+      };
+
+      const result = await ExamModel.create(examData);
+
+      return result;
+    } catch (error) {
+      throw new Error(`Failed to create exam: ${error}`);
+    }
+  }
+
+  async getAllExams(query: any) {
+    const page = Number(query.page) || 1;
+    const limit = Number(query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const searchTerm = query.searchTerm || "";
+
+    const searchCondition = {
+      ...(searchTerm && { title: { $regex: searchTerm, $options: "i" } }),
+    };
+
+    const exams = await ExamModel.find(searchCondition)
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    const total = await ExamModel.countDocuments(searchCondition);
+
+    return {
+      meta: {
+        page,
+        limit,
+        total,
+        totalPage: Math.ceil(total / limit),
+      },
+      data: exams,
+    };
+  }
+
+  async getExamById(id: string) {
+    const exam = await ExamModel.findOne({ exam_number: id }).populate(
+      "questions"
+    );
+    return exam;
+  }
+
+  async updateExamById(id: string, updateData: any) {
+    const updatedExam = await ExamModel.findOneAndUpdate(
+      { exam_number: id },
+      updateData,
+      { new: true }
+    );
+    return updatedExam;
+  }
+
+  async deleteExamById(id: string) {
+    const deletedExam = await ExamModel.findOneAndDelete({
+      exam_number: id,
+    });
+    return deletedExam;
+  }
+
+  async toggleExamStatus(id: string) {
+    const result = await ExamModel.findOneAndUpdate(
+      { exam_number: id },
+      [
+        {
+          $set: {
+            status: {
+              $cond: {
+                if: { $eq: ["$status", GUIDELINE_STATUS.ACTIVE] },
+                then: GUIDELINE_STATUS.INACTIVE,
+                else: GUIDELINE_STATUS.ACTIVE,
+              },
+            },
+          },
+        },
+      ],
+      { new: true }
+    );
+
+    return result;
+  }
+}
+
+export const examService = new Service();
