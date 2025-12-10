970b0b63f3258f613f4a299baca540a64a8ec64b
engrsakib <snazmussakib01@gmail.com>
2025-12-11 03:29:17 +0600

    results data set ready

--- src/modules/results/result.controller.ts
+++ src/modules/results/result.controller.ts
@@ -14,67 +14,18 @@ class Controller extends BaseController {
     });
   });
 
-  //   getAllQuestions = this.catchAsync(async (req: Request, res: Response) => {
-  //     // ১. ফিল্টার আলাদা করা
-  //     const filters = {
-  //       searchTerm: req.query.searchTerm as string,
-  //     };
+  getResultsBySearch = this.catchAsync(async (req: Request, res: Response) => {
+    const phone = req.query.phone as string | undefined;
+    const examNum = req.query.examNum ? Number(req.query.examNum) : undefined;
 
-  //     // ২. পেজিনেশন অপশন (এখানেই ফিক্স করা হয়েছে)
-  //     const paginationOptions = {
-  //       // Number(...) ব্যবহার করে স্ট্রিং থেকে নাম্বারে কনভার্ট করুন
-  //       page: Number(req.query.page || 1),
-  //       limit: Number(req.query.limit || 10),
-  //       sortBy: req.query.sortBy as string,
-  //       sortOrder: req.query.sortOrder as "asc" | "desc",
-  //     };
-
-  //     const result = await QuestionService.getAllQuestions(
-  //       filters,
-  //       paginationOptions
-  //     );
-
-  //     this.sendResponse(res, {
-  //       statusCode: HttpStatusCode.OK,
-  //       success: true,
-  //       message: "Questions retrieved successfully",
-
-  //       data: result,
-  //     });
-  //   });
-
-  //   getQuestionById = this.catchAsync(async (req: Request, res: Response) => {
-  //     const id = Number(req.params.id);
-  //     const question = await QuestionService.getQuestionById(id);
-  //     this.sendResponse(res, {
-  //       statusCode: HttpStatusCode.OK,
-  //       success: true,
-  //       message: "Question retrieved successfully",
-  //       data: question,
-  //     });
-  //   });
-
-  //   updateQuestionById = this.catchAsync(async (req: Request, res: Response) => {
-  //     const id = Number(req.params.id);
-  //     const question = await QuestionService.updateQuestionById(id, req.body);
-  //     this.sendResponse(res, {
-  //       statusCode: HttpStatusCode.OK,
-  //       success: true,
-  //       message: "Question updated successfully",
-  //       data: question,
-  //     });
-  //   });
-
-  //   deleteQuestionById = this.catchAsync(async (req: Request, res: Response) => {
-  //     const id = Number(req.params.id);
-  //     await QuestionService.deleteQuestionById(id);
-  //     this.sendResponse(res, {
-  //       statusCode: HttpStatusCode.OK,
-  //       success: true,
-  //       message: "Question deleted successfully",
-  //       data: null,
-  //     });
-  //   });
+    const results = await resultService.getResultsBySearch(phone, examNum);
+    this.sendResponse(res, {
+      statusCode: HttpStatusCode.OK,
+      success: true,
+      message: "Results retrieved successfully",
+      data: results,
+    });
+  });
 }
 
 export const ResultController = new Controller();
