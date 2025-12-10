ed5d8b7d90efc95b4d163989948fd88f2afe427a
engrsakib <snazmussakib01@gmail.com>
2025-12-11 03:27:13 +0600

    data query

--- src/modules/results/result.service.ts
+++ src/modules/results/result.service.ts
@@ -5,6 +5,41 @@ class service {
     const result = await ResultModel.create(resultData);
     return result;
   };
+
+  getResultsBySearch = async (phone?: string, examNum?: number) => {
+    const matchQuery: any = {};
+
+    if (phone) {
+      matchQuery.student_phone = phone;
+    }
+    if (examNum) {
+      matchQuery.exam_number = examNum;
+    }
+
+    const result = await ResultModel.aggregate([
+      {
+        $match: matchQuery,
+      },
+
+      {
+        $sort: {
+          score: -1,
+        },
+      },
+
+      {
+        $project: {
+          _id: 0,
+          student_name: 1,
+          student_phone: 1,
+          exam_number: 1,
+          score: 1,
+        },
+      },
+    ]);
+
+    return result;
+  };
 }
 
 export const resultService = new service();
