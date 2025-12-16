656fab57f6ec8a785a107f88662e9419fd2184dd
engrsakib <snazmussakib01@gmail.com>
2025-12-16 12:30:02 +0600

    get all user ready

--- src/modules/youtube/youtube.service.ts
+++ src/modules/youtube/youtube.service.ts
@@ -18,6 +18,33 @@ class Service {
     const searchTerm = query.searchTerm || "";
 
     const searchCondition = {
+      ...(searchTerm && { title: { $regex: searchTerm, $options: "i" } }),
+    };
+
+    const videos = await YoutubeModel.find(searchCondition)
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    const total = await YoutubeModel.countDocuments(searchCondition);
+
+    return {
+      meta: {
+        page,
+        limit,
+        total,
+        totalPage: Math.ceil(total / limit),
+      },
+      data: videos,
+    };
+  }
+  async getAllYoutubeVideosForUsers(query: any) {
+    const page = Number(query.page) || 1;
+    const limit = Number(query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const searchTerm = query.searchTerm || "";
+
+    const searchCondition = {
       is_published: true,
       ...(searchTerm && { title: { $regex: searchTerm, $options: "i" } }),
     };
