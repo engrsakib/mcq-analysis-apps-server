78c5db17b248f503e827673f8ecaa9150d4ccf36
engrsakib <snazmussakib01@gmail.com>
2026-02-01 18:17:40 +0600

    uodate

--- src/modules/youtube/youtube.interface.ts
+++ src/modules/youtube/youtube.interface.ts
@@ -1,7 +1,7 @@
 export type IYoutube = {
   video_number?: number;
-  thumbnail_url: string;
-  title: string;
+  thumbnail_url?: string;
+  title?: string;
   video_url: string;
   description?: string;
   is_published: boolean;
--- src/modules/youtube/youtube.model.ts
+++ src/modules/youtube/youtube.model.ts
@@ -3,8 +3,8 @@ import { IYoutube } from "./youtube.interface";
 
 const youtubeSchema = new Schema<IYoutube>({
   video_number: { type: Number, required: false },
-  title: { type: String, required: true },
-  thumbnail_url: { type: String, required: true },
+  title: { type: String, required: false },
+  thumbnail_url: { type: String, required: false },
   video_url: { type: String, required: true },
   description: { type: String, required: false },
   is_published: { type: Boolean, required: true, default: false },
