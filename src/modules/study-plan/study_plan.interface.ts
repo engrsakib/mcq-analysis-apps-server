fe0ad7ee3836c232d9fab27d4a88b7a53f4979c5
engrsakib <snazmussakib01@gmail.com>
2026-02-12 14:44:30 +0600

    feat: add study plan interface and guideline enums

---
+++ src/modules/study-plan/study_plan.interface.ts
@@ -0,0 +1,23 @@
+export type IstudyPlan = {
+  study_plan_number?: number;
+  title: string;
+  description: string;
+  status: GUIDELINE_STATUS;
+  thumbnail_url?: string;
+  study_plan_url: string;
+};
+
+export enum GUIDELINE_STATUS {
+  INACTIVE = "inactive",
+  ACTIVE = "active",
+  ADMIN_APPROVAL = "admin_approval",
+}
+
+export enum GUIDELINE_CATEGORY_ENUMS {
+  GENERAL = "general",
+  TECHNICAL = "technical",
+  EXAM = "exam",
+  BCS_PREPARATION = "bcs_preparation",
+  PRIMARY_TEACHER_PREPARATION = "primary_teacher_preparation",
+  TEACHER_NIBONDHON_PREPARATION = "teacher_nibondhon_preparation",
+}
